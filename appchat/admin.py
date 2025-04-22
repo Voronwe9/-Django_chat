b18a8cfaeed29e360397833fa4789086323d2b1a
Barynin Anton <118472119+Voronwe9@users.noreply.github.com>
2025-04-22 09:44:06 +0300

    Update admin.py
    докстринги для админки

--- appchat/admin.py
+++ appchat/admin.py
@@ -5,9 +5,15 @@ from .models import Post, User
 from django.contrib.auth.admin import UserAdmin
 
 
-# 1. Регистрируем кастомную модель User с UserAdmin
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
+    """
+    Админка для кастомной модели пользователя.
+
+    Наследуется от стандартного UserAdmin, добавляет:
+    - отображение номера телефона и даты рождения;
+    - отображение в списке полей: username, email, birth_date, is_staff.
+    """
     list_display = ("username", "email", "birth_date", "is_staff")
     fieldsets = UserAdmin.fieldsets + (
         ("Дополнительно", {"fields": ("phone", "birth_date")}),
@@ -16,24 +22,41 @@ class CustomUserAdmin(UserAdmin):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    """
+    Админка для модели Post.
+
+    Функционал:
+    - отображение заголовка, ссылки на автора, даты создания;
+    - фильтрация по дате создания (в т.ч. с календарём);
+    - поиск по заголовку и имени автора;
+    - сортировка по имени автора.
+    """
     list_display = ["title", "author_link", "created_at"]
 
-    # Фильтрация по дате создания
+
     list_filter = [
-        "created_at",  # Простой фильтр по дате
+        "created_at",  
         ("created_at", admin.DateFieldListFilter),
     ]
 
-    # Навигация по датам (календарь над списком)
+
     date_hierarchy = "created_at"
 
-    # Кликабельная ссылка на автора
     def author_link(self, obj):
+    """
+    Возвращает HTML-ссылку на страницу редактирования автора поста.
+
+    Args:
+        obj (Post): объект поста.
+
+    Returns:
+        str: HTML-ссылка на страницу пользователя в админке.
+    """
         url = reverse("admin:appchat_user_change", args=[obj.author.id])
         return format_html('<a href="{}">{}</a>', url, obj.author.username)
 
     author_link.short_description = "Автор"
-    author_link.admin_order_field = "author__username"  # Сортировка по автору
+    author_link.admin_order_field = "author__username" 
 
-    # Поиск по заголовку и автору
+
     search_fields = ["title", "author__username"]
