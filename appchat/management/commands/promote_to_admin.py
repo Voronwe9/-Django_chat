a16a2c9401c20d5d8161356d66a352bcc455dc74
Barynin Anton <118472119+Voronwe9@users.noreply.github.com>
2025-04-28 10:03:07 +0300

    Update promote_to_admin.py
    добавил докстринги в админку

--- appchat/management/commands/promote_to_admin.py
+++ appchat/management/commands/promote_to_admin.py
@@ -3,12 +3,37 @@ from django.contrib.auth import get_user_model
 
 
 class Command(BaseCommand):
+    """
+    Кастомная команда управления для назначения пользователя администратором.
+
+    Позволяет назначить указанного пользователя администратором системы,
+    устанавливая флаги is_staff и is_superuser в True.
+
+    Пример использования:
+        python manage.py make_admin username
+    """
     help = "Назначает пользователя администратором"
 
     def add_arguments(self, parser):
+        """
+        Добавляет аргументы для команды.
+
+        Args:
+            parser: Парсер аргументов командной строки.
+        """
         parser.add_argument("username", type=str)
 
     def handle(self, *args, **options):
+        """
+        Основная логика команды.
+
+        Получает пользователя по имени, устанавливает права администратора
+        и сохраняет изменения.
+
+        Args:
+            *args: Дополнительные позиционные аргументы.
+            **options: Аргументы командной строки.
+        """
         User = get_user_model()
         user = User.objects.get(username=options["username"])
         user.is_staff = True
